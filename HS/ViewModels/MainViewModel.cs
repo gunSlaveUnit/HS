b41b2a131c79877bdfa7eea6040de98e5651d01a
ellmac <rp34789t5409yvt@protonmail.com>
2021-11-16 13:00:52 +0300

    In main view model created one prop to set current view model and two commands to change view models

--- HS/ViewModels/MainViewModel.cs
+++ HS/ViewModels/MainViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Linq;
+using System.Windows.Input;
 using Hotel.Context.Entities;
+using Hotel.Infrastructure.Commands.Base;
 using Hotel.Interfaces;
 using HS.Services;
 using HS.ViewModels.Base;
@@ -9,6 +11,55 @@ namespace HS.ViewModels
 {
     public class MainViewModel : ViewModel
     {
+        #region Commands
+
+        #region ShowClientsView
+
+        private ICommand _showClientsViewCommand;
+
+        public ICommand ShowClientsViewCommand => _showClientsViewCommand
+            ??= new RelayCommand(OnShowClientsViewCommandExecute, CanShowClientsViewCommandExecute);
+        
+        private bool CanShowClientsViewCommandExecute(object parameter) => true;
+
+        private void OnShowClientsViewCommandExecute(object parameter)
+        {
+            
+        }
+
+        #endregion
+        #region ShowReservationsView
+
+        private ICommand _showReservationsViewCommand;
+
+        public ICommand ShowReservationsViewCommand => _showReservationsViewCommand
+            ??= new RelayCommand(OnShowClientsViewCommandExecute, CanShowClientsViewCommandExecute);
+        
+        private bool CanShowReservationsViewCommandExecute(object parameter) => true;
+
+        private void OnShowReservationsViewCommandExecute(object parameter)
+        {
+            
+        }
+
+        #endregion
+        
+        #endregion
+        #region Properties
+
+        #region CurrentViewModel
+
+        private ViewModel _currentViewModel;
+
+        public ViewModel CurrentViewModel
+        {
+            get => _currentViewModel;
+            private set => Set(ref _currentViewModel, value);
+        }
+
+        #endregion
+        
+        #endregion
         public MainViewModel(IRepository<Client> clientsRepository, IBookingService bookingService)
         {
             var items = clientsRepository.All.ToArray();
---
+++ HS/Views/Pages/ClientsView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace HS.Views.Pages
+{
+    public partial class ClientsView : UserControl
+    {
+        public ClientsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
