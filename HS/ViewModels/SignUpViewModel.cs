4999ad05adccf306ba0da014d3469f7418ade81d
ellmac <rp34789t5409yvt@protonmail.com>
2021-11-17 00:22:25 +0300

    Created all properties for client. Now it can sign up a client

--- HS/ViewModels/SignUpViewModel.cs
+++ HS/ViewModels/SignUpViewModel.cs
@@ -1,12 +1,101 @@
-﻿using HS.ViewModels.Base;
+﻿using System.Windows;
+using System.Windows.Input;
+using Hotel.Views.Windows;
+using HS.Infrastructure.Commands.Base;
+using HS.Services;
+using HS.ViewModels.Base;
 
 namespace HS.ViewModels
 {
     public class SignUpViewModel : ViewModel
     {
-        public SignUpViewModel()
+        private readonly IClientService _clientService;
+
+        private string _surname;
+
+        public string Surname
         {
-            
+            get => _surname;
+            set => Set(ref _surname, value);
+        }
+        
+        private string _name;
+
+        public string Name
+        {
+            get => _name;
+            set => Set(ref _name, value);
+        }
+        
+        private string _patronymic;
+
+        public string Patronymic
+        {
+            get => _patronymic;
+            set => Set(ref _patronymic, value);
+        }
+        
+        private string _passport;
+
+        public string Passport
+        {
+            get => _passport;
+            set => Set(ref _passport, value);
+        }
+        
+        private string _phoneNumber;
+
+        public string PhoneNumber
+        {
+            get => _phoneNumber;
+            set => Set(ref _phoneNumber, value);
+        }
+        
+        private string _login;
+
+        public string Login
+        {
+            get => _login;
+            set => Set(ref _login, value);
+        }
+        
+        private string _password;
+
+        public string Password
+        {
+            get => _password;
+            set => Set(ref _password, value);
+        }
+        
+        private string _passwordConfirm;
+
+        public string PasswordConfirm
+        {
+            get => _passwordConfirm;
+            set => Set(ref _passwordConfirm, value);
+        }
+        
+        #region NewUser
+
+        private ICommand _signUpCommand;
+        
+        public ICommand SignUpCommand => _signUpCommand
+            ??= new RelayCommand(OnSignUpCommandExecuted, CanSignUpCommandExecute);
+
+        private void OnSignUpCommandExecuted(object p)
+        {
+            var currentClient = _clientService.SignUp(
+                Surname, Name, Patronymic, Passport, PhoneNumber, Login, Password
+                );
+        }
+
+        private bool CanSignUpCommandExecute(object p) => Password == PasswordConfirm;
+
+        #endregion
+        
+        public SignUpViewModel(IClientService clientService)
+        {
+            _clientService = clientService;
         }
     }
 }
