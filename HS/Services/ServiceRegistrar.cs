ba7914d31b83935af3e230cd1afe42c7ef2ef80b
ellmac <rp34789t5409yvt@protonmail.com>
2021-11-16 10:57:07 +0300

    Booking service was added to registrar

--- HS/Services/ServiceRegistrar.cs
+++ HS/Services/ServiceRegistrar.cs
@@ -5,6 +5,7 @@ namespace HS.Services
     public static class ServiceRegistrar
     {
         public static IServiceCollection AddServices(this IServiceCollection services) => services
+            .AddTransient<BookingService>()
         ;
     }
 }
