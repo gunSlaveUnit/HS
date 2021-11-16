3de06193fc0c5ef256ac336a6c7858acbe852081
ellmac <rp34789t5409yvt@protonmail.com>
2021-11-16 13:16:57 +0300

    Added reservation view

---
+++ HS/Views/Pages/ReservationsView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace HS.Views.Pages
+{
+    public partial class ReservationsView : UserControl
+    {
+        public ReservationsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
