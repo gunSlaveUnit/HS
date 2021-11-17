fa86034bf0eba7baae333c6cebcff88ecc24da01
ellmac <rp34789t5409yvt@protonmail.com>
2021-11-17 18:21:12 +0300

    In clients view added a datagrid and it can show clients

--- HS/ViewModels/ClientsViewModel.cs
+++ HS/ViewModels/ClientsViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Hotel.Context.Entities;
+﻿using System.Linq;
+using Hotel.Context.Entities;
 using Hotel.Interfaces;
 using HS.ViewModels.Base;
 
@@ -6,9 +7,19 @@ namespace HS.ViewModels
 {
     public class ClientsViewModel : ViewModel
     {
+        private IRepository<Client> _clientsRepository;
+        private IQueryable<Client> _clients;
+
+        public IQueryable<Client> Clients
+        {
+            get => _clients;
+            set => Set(ref _clients, value);
+        }
+
         public ClientsViewModel(IRepository<Client> clientsRepository)
         {
-            
+            _clientsRepository = clientsRepository;
+            Clients = _clientsRepository.All;
         }
     }
 }
