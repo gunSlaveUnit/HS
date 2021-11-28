534a07ad690a49a5bfb41e7f89f42b2ab0f3f857
ellmac <rp34789t5409yvt@protonmail.com>
2021-11-28 20:57:40 +0300

    Added room type model

---
+++ HS/Context/Entities/RoomType.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using HS.Context.Entities.Base;
+
+namespace HS.Context.Entities
+{
+    public class RoomType : Entity
+    {
+        public int Capacity { get; set; }
+        public int CostPerHour { get; set; }
+        public int CostPerDay { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public virtual ICollection<Room> Rooms { get; set; }
+    }
+}
