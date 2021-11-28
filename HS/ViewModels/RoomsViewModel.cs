ef5b729a1097987f5353e5dd7dadcd40b8955dbb
ellmac <rp34789t5409yvt@protonmail.com>
2021-11-28 21:39:37 +0300

    Added list of rooms and repository to rooms vm

--- HS/ViewModels/RoomsViewModel.cs
+++ HS/ViewModels/RoomsViewModel.cs
@@ -1,12 +1,26 @@
-﻿using HS.ViewModels.Base;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Hotel.Interfaces;
+using HS.Context.Entities;
+using HS.ViewModels.Base;
 
 namespace HS.ViewModels
 {
     public class RoomsViewModel : ViewModel
     {
-        public RoomsViewModel()
+        private IRepository<Room> _roomsRepository;
+        private List<Room> _rooms;
+
+        public List<Room> Rooms
         {
-            
+            get => _rooms;
+            set => Set(ref _rooms, value);
+        }
+
+        public RoomsViewModel(IRepository<Room> roomsRepository)
+        {
+            _roomsRepository = roomsRepository;
+            Rooms = _roomsRepository.All.ToList();
         }
     }
 }
