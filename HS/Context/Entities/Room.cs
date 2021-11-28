9ede1d2f4211118ec5d402ed1cb85f8b6ed39ce1
ellmac <rp34789t5409yvt@protonmail.com>
2021-11-28 20:57:48 +0300

    added a room model

---
+++ HS/Context/Entities/Room.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using HS.Context.Entities.Base;
+
+namespace HS.Context.Entities
+{
+    public class Room : Entity
+    {
+        [Required]
+        public int Number { get; set; }
+        public int RoomTypeId { get; set; }
+        public virtual RoomType RoomType { get; set; }
+    }
+}
