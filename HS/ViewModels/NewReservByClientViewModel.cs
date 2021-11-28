2f468c36e8447cd4a3b2d79bca2b321d8a8033dc
ellmac <rp34789t5409yvt@protonmail.com>
2021-11-28 14:39:50 +0300

    To new reserv vm were added one command to create a reserv and two prop for dates

--- HS/ViewModels/NewReservByClientViewModel.cs
+++ HS/ViewModels/NewReservByClientViewModel.cs
@@ -1,7 +1,49 @@
-﻿namespace HS.ViewModels
+﻿using System;
+using System.Windows.Input;
+using Hotel.Context.Entities;
+using HS.Infrastructure.Commands.Base;
+using HS.Services;
+using HS.ViewModels.Base;
+
+namespace HS.ViewModels
 {
-    public class NewReservByClientViewModel
+    public class NewReservByClientViewModel : ViewModel
     {
+        private readonly IBookingService _bookingService;
         
+        private DateTime _arrivalDate;
+
+        public DateTime ArrivalDate
+        {
+            get => _arrivalDate;
+            set => Set(ref _arrivalDate, value);
+        }
+        
+        private DateTime _departureDate;
+
+        public DateTime DepartureDate
+        {
+            get => _departureDate;
+            set => Set(ref _departureDate, value);
+        }
+        
+        private ICommand _createNewReservCommand;
+        private readonly ViewModelLocator _locator;
+
+        public ICommand CreateNewReservCommand => _createNewReservCommand
+            ??= new RelayCommand(OnCreateNewReservCommandExecuted, CanCreateNewReservCommandExecute);
+        
+        private bool CanCreateNewReservCommandExecute(object parameter) => true;
+
+        private void OnCreateNewReservCommandExecuted(object parameter)
+        {
+            _bookingService.Reservate(_locator.MainViewModel.CurrentUser, ArrivalDate, DepartureDate);
+        }
+
+        public NewReservByClientViewModel(ViewModelLocator locator, IBookingService bookingService)
+        {
+            _bookingService = bookingService;
+            _locator = locator;
+        }
     }
 }
