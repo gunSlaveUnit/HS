a2615deb2dc5470ec98a272292b8e1e655fc23e7
ellmac <rp34789t5409yvt@protonmail.com>
2021-12-24 22:43:37 +0300

    Added orderes services entity

---
+++ HS/Context/Entities/OrderedServices.cs
@@ -0,0 +1,9 @@
+﻿namespace HS.Context.Entities
+{
+    public class OrderedServices
+    {
+        public int ClientId { get; set; }
+        public int ReservationId { get; set; }
+        public int CheckoutPrice { get; set; }
+    }
+}
