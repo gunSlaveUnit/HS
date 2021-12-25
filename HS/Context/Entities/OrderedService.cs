e68241aef6b6f3e2052c71fa56171b9b6185c01e
ellmac <rp34789t5409yvt@protonmail.com>
2021-12-25 11:44:28 +0300

    Added two virtual entity properties: Service and Reservation in OrderedService

--- HS/Context/Entities/OrderedService.cs
+++ HS/Context/Entities/OrderedService.cs
@@ -4,8 +4,10 @@ namespace HS.Context.Entities
 {
     public class OrderedService : Entity
     {
-        public int ClientId { get; set; }
+        public int ServiceId { get; set; }
+        public virtual Service Service { get; set; }
         public int ReservationId { get; set; }
+        public virtual Reservation Reservation { get; set; }
         public int CheckoutPrice { get; set; }
     }
 }
