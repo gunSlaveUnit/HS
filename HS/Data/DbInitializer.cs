ed9f67379d8bf02b015e485ce70ec73f0791b0d2
ellmac <rp34789t5409yvt@protonmail.com>
2021-12-25 12:14:21 +0300

    In DbInitializer were added methods to init services and admin profile

--- HS/Data/DbInitializer.cs
+++ HS/Data/DbInitializer.cs
@@ -1,5 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using HS.Context;
+using HS.Context.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
@@ -18,9 +20,50 @@ namespace HS.Data
 
         public async Task Initialize()
         {
-            //await _context.Database.EnsureDeletedAsync();
+            await _context.Database.EnsureDeletedAsync();
             await _context.Database.EnsureCreatedAsync().ConfigureAwait(false);
             await _context.Database.MigrateAsync();
+
+            await InitializeAdminProfile();
+            await InitializeServices();
+        }
+
+        private async Task InitializeAdminProfile()
+        {
+            Client admin = new Client
+            {
+                Surname = "Тямин",
+                Name = "Александр",
+                Patronymic = "Дмитриевич",
+                Document = "2014567398",
+                PhoneNumber = "89065412732",
+                Login = "admin",
+                Password = "E550BC898ED418254B8B32A284A333658CF0DC32FFD559665E102B360593C466",
+                Salt = "uQOOOMV7ds",
+            };
+            await _context.Clients.AddAsync(admin);
+            await _context.SaveChangesAsync();
+        }
+        private async Task InitializeServices()
+        {
+            string[] titles = new[] {"Gym", "Swimming Pool", "Food"};
+            string[] descriptions = new[]
+            {
+                "Круглосуточный зал со множеством тренажеров",
+                "Наш бассейн подойдет как желающим просто отдохнуть, так и профессиональным пловцам",
+                "Трехразовое питание. Все блюда на Ваш выбор",
+            };
+            int[] prices = new[] {180, 520, 300};
+            int servicesCount = 3;
+            Service[] services = Enumerable.Range(0, servicesCount)
+                .Select(s => new Service
+                {
+                    Title = titles[s],
+                    Description = descriptions[s],
+                    Price = prices[s],
+                }).ToArray();
+            await _context.Services.AddRangeAsync(services);
+            await _context.SaveChangesAsync();
         }
     }
 }
