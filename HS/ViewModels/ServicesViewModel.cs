94c305838423d62f91836f44e0730d63903ffe81
ellmac <rp34789t5409yvt@protonmail.com>
2021-12-25 14:04:41 +0300

    Added services view model

---
+++ HS/ViewModels/ServicesViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Hotel.Interfaces;
+using HS.Context.Entities;
+using HS.ViewModels.Base;
+
+namespace HS.ViewModels
+{
+    public class ServicesViewModel : ViewModel
+    {
+        private readonly ViewModelLocator _locator;
+        private readonly IRepository<Service> _servicesRepository;
+
+        public ServicesViewModel(ViewModelLocator locator,
+            IRepository<Service> servicesRepository)
+        {
+            _locator = locator;
+            _servicesRepository = servicesRepository;
+        }
+    }
+}
