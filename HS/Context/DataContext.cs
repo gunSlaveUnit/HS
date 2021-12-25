1a38ddf79646ae0b9a39bc3f58dddd9690291ebc
ellmac <rp34789t5409yvt@protonmail.com>
2021-12-25 12:13:50 +0300

    DbSets are public now

--- HS/Context/DataContext.cs
+++ HS/Context/DataContext.cs
@@ -5,12 +5,12 @@ namespace HS.Context
 {
     public class DataContext : DbContext
     {
-        private DbSet<Client> Clients { get; set; }
-        private DbSet<Reservation> Reservations { get; set; }
-        private DbSet<Room> Rooms { get; set; }
-        private DbSet<RoomType> RoomTypes { get; set; }
-        private DbSet<Service> Services { get; set; }
-        private DbSet<OrderedService> OrderedServices { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<RoomType> RoomTypes { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<OrderedService> OrderedServices { get; set; }
 
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
