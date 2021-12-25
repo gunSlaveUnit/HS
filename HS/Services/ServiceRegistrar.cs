886df75abeefbb57f965a559ee32c55e0654b109
ellmac <rp34789t5409yvt@protonmail.com>
2021-12-25 20:59:59 +0300

    Created a statistics service

--- HS/Services/ServiceRegistrar.cs
+++ HS/Services/ServiceRegistrar.cs
@@ -7,6 +7,7 @@ namespace HS.Services
         public static IServiceCollection AddServices(this IServiceCollection services) => services
             .AddTransient<IBookingService, BookingService>()
             .AddTransient<IClientService, ClientService>()
+            .AddTransient<IStatisticsService, StatisticsService>()
         ;
     }
 }
---
+++ HS/Services/StatisticsService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Hotel.Interfaces;
+using HS.Context.Entities;
+
+namespace HS.Services
+{
+    public class StatisticsService : IStatisticsService
+    {
+        private readonly IRepository<Reservation> _reservationsRepository;
+        private readonly IRepository<OrderedService> _orderedServicesRepository;
+
+        public StatisticsService(IRepository<Reservation> reservationsRepository,
+            IRepository<OrderedService> orderedServicesRepository)
+        {
+            _reservationsRepository = reservationsRepository;
+            _orderedServicesRepository = orderedServicesRepository;
+        }
+
+        public ObservableCollection<Reservation> GetReservationsByTime(DateTime lowerBound, DateTime upperBound)
+            => new ObservableCollection<Reservation>(_reservationsRepository.All.Where(
+                r => r.ArrivalDate >= lowerBound && r.DepartureDate <= upperBound));
+
+        public int GetIncome(DateTime lowerBound, DateTime upperBound)
+            => _reservationsRepository.All.Sum(r => r.Cost) 
+               + _orderedServicesRepository.All.Sum(o => o.CheckoutPrice);
+    }
+}
