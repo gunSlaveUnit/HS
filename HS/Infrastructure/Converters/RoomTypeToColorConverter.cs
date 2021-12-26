5cc710a531db4cf88fdc6e39c12b20e3c169fee4
ellmac <rp34789t5409yvt@protonmail.com>
2021-12-26 13:24:44 +0300

    Trying to add a converter between room type and color

---
+++ HS/Infrastructure/Converters/RoomTypeToColorConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+using HS.Context.Entities;
+
+namespace HS.Infrastructure.Converters
+{
+    public class RoomTypeToColorConverter : IValueConverter
+    {
+        public object Convert(object v, Type t, object p, CultureInfo c)
+        {
+            SolidColorBrush color = null;
+            var type = (string) v;
+            if (type == "Business") color = new SolidColorBrush(Colors.DodgerBlue);
+            if (type == "Honeymoon") color = new SolidColorBrush(Colors.Green);
+            if (type == "Family") color = new SolidColorBrush(Colors.Coral);
+            if (type == "Lux") color = new SolidColorBrush(Colors.Gold);
+            return color;
+        }
+
+        public object ConvertBack(object v, Type t, object p, CultureInfo c)
+        {
+            return null;
+        }
+    }
+}
