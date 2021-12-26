665b07b185abfa5f868d92646c76aa347cbce5bc
ellmac <rp34789t5409yvt@protonmail.com>
2021-12-26 12:04:29 +0300

    Added properties to show statistics

--- HS/ViewModels/StatisticsViewModel.cs
+++ HS/ViewModels/StatisticsViewModel.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Linq;
+using System.Windows.Input;
+using Hotel.Interfaces;
+using HS.Context.Entities;
+using HS.Infrastructure.Commands.Base;
 using HS.Services;
 using HS.ViewModels.Base;
 
@@ -7,12 +12,36 @@ namespace HS.ViewModels
     public class StatisticsViewModel : ViewModel
     {
         private readonly IStatisticsService _statisticsService;
-        private int _count;
-        public int Count
+        private int _reservationsAmountInCurrentMonth;
+
+        public int ReservationsAmountInCurrentMonth
         {
-            get => _count;
-            set => Set(ref _count, value);
+            get => _reservationsAmountInCurrentMonth;
+            set => Set(ref _reservationsAmountInCurrentMonth, value);
         }
+        
+        private int _reservationsAmountInPreviousMonth;
+
+        public int ReservationsAmountInPreviousMonth
+        {
+            get => _reservationsAmountInPreviousMonth;
+            set => Set(ref _reservationsAmountInPreviousMonth, value);
+        }
+        
+        private int _incomeCurrentMonth;
+        public int IncomeCurrentMonth
+        {
+            get => _incomeCurrentMonth;
+            set => Set(ref _incomeCurrentMonth, value);
+        }
+        
+        private int _incomePreviousMonth;
+        public int IncomePreviousMonth
+        {
+            get => _incomePreviousMonth;
+            set => Set(ref _incomeCurrentMonth, value);
+        }
+        
         private DateTime _lowerBound;
 
         public DateTime LowerBound
@@ -21,16 +50,48 @@ namespace HS.ViewModels
             set => Set(ref _lowerBound, value);
         }
         private DateTime _upperBound;
+        private readonly IRepository<Reservation> _reservationsRepository;
 
         public DateTime UpperBound
         {
             get => _upperBound;
             set => Set(ref _upperBound, value);
         }
-        public StatisticsViewModel(IStatisticsService statisticsService)
+
+        private void Update()
+        {
+            DateTime lowerBound = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1);
+            DateTime upperBound = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 
+                DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month));
+            IncomeCurrentMonth = _statisticsService.GetIncome(lowerBound, upperBound);
+            IncomePreviousMonth = _statisticsService.GetIncome(
+                new DateTime(DateTime.Now.Year, DateTime.Now.Month - 1, 1),
+                new DateTime(DateTime.Now.Year, DateTime.Now.Month - 1,
+                    DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month - 1)));
+            ReservationsAmountInCurrentMonth = _statisticsService.GetReservationsByTime(
+                lowerBound, upperBound).Count;
+            ReservationsAmountInPreviousMonth = _statisticsService.GetReservationsByTime(
+                new DateTime(DateTime.Now.Year, DateTime.Now.Month-1, 1),
+                new DateTime(DateTime.Now.Year, DateTime.Now.Month-1,
+                    DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month-1))).Count;
+        }
+        
+        private ICommand _updateStatistics;
+        public ICommand UpdateStatisticsCommand => _updateStatistics
+            ??= new RelayCommand(OnUpdateStatisticsCommandExecuted, CanUpdateStatisticsCommandExecute);
+
+        private bool CanUpdateStatisticsCommandExecute(object p) => true;
+
+        private void OnUpdateStatisticsCommandExecuted(object p) => Update();
+        public StatisticsViewModel(IStatisticsService statisticsService,
+            IRepository<Reservation> reservationsRepository)
         {
             _statisticsService = statisticsService;
-            Count = _statisticsService.GetIncome(LowerBound, UpperBound);
+            _reservationsRepository = reservationsRepository;
+
+            LowerBound = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1);
+            UpperBound = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 
+                DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month));
         }
     }
 }
