050bcc09c7643412a80d0fa40d06c17516a4dcf0
ellmac <rp34789t5409yvt@protonmail.com>
2021-12-26 21:56:26 +0300

    Added a view to display room types

---
+++ HS/ViewModels/RoomTypesViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Hotel.Interfaces;
+using HS.Context.Entities;
+using HS.ViewModels.Base;
+
+namespace HS.ViewModels
+{
+    public class RoomTypesViewModel : ViewModel
+    {
+        private IRepository<RoomType> _roomTypesRepository;
+        
+        private List<RoomType> _roomTypes;
+
+        public List<RoomType> RoomTypes
+        {
+            get => _roomTypes;
+            set => Set(ref _roomTypes, value);
+        }
+
+        public RoomTypesViewModel(IRepository<RoomType> roomTypesRepository)
+        {
+            _roomTypesRepository = roomTypesRepository;
+            RoomTypes = _roomTypesRepository.All.ToList();
+        }
+    }
+}
