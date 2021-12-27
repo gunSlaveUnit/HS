07b01fdc8abb8ccbda85d2dec24d6abf67943927
ellmac <rp34789t5409yvt@protonmail.com>
2021-12-27 09:04:52 +0300

    Employee can register a new client

--- HS/ViewModels/ClientsViewModel.cs
+++ HS/ViewModels/ClientsViewModel.cs
@@ -1,8 +1,12 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Windows;
+using System.Windows.Input;
 using Hotel.Interfaces;
+using Hotel.Views.Windows;
 using HS.Context.Entities;
+using HS.Infrastructure.Commands.Base;
 using HS.ViewModels.Base;
 
 namespace HS.ViewModels
@@ -17,6 +21,53 @@ namespace HS.ViewModels
             get => _clients;
             set => Set(ref _clients, value);
         }
+
+        private Client _selectedClient;
+
+        public Client SelectedClient
+        {
+            get => _selectedClient;
+            set => Set(ref _selectedClient, value);
+        }
+        
+        #region Commands
+
+        #region CreateNewClientCommand
+        
+        private ICommand _createNewClientCommand;
+        public ICommand CreateNewClientCommand => _createNewClientCommand
+            ??= new RelayCommand(OnCreateNewClientCommandExecuted, CanCreateNewClientCommandExecute);
+        
+        private bool CanCreateNewClientCommandExecute(object p) => true;
+
+        private void OnCreateNewClientCommandExecuted(object p)
+        {
+            var newUserWindow = new SignUpWindow();
+            newUserWindow.Owner = Application.Current.MainWindow;
+            newUserWindow.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            newUserWindow.ShowDialog();
+        }
+        
+        #endregion
+        
+        #region DeleteClientCommand
+        
+        private ICommand _deleteClientCommand;
+        public ICommand DeleteClientCommand => _deleteClientCommand
+            ??= new RelayCommand(OnDeleteClientCommandExecuted, CanDeleteClientCommandExecute);
+        
+        private bool CanDeleteClientCommandExecute(object p) => p is RoomType;
+
+        private void OnDeleteClientCommandExecuted(object p)
+        {
+            if (p is not RoomType) return;
+            var r = (RoomType) p;
+            //_roomTypesRepository.DeleteBy(r.Id);
+        }
+        
+        #endregion
+        
+        #endregion
 
         public ClientsViewModel(IRepository<Client> clientsRepository)
         {
